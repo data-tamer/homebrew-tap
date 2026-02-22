94c43dede0bead1e8f0cbe6b07fbce5549cb21d2
riccardo <riccardo.ravaro@datatamer.ai>
2026-02-22 15:35:58 +0700

    agentdev 1.0.18

--- Formula/agentdev.rb
+++ Formula/agentdev.rb
@@ -1,8 +1,8 @@
 class Agentdev < Formula
   desc "AgentDev distributed agent client"
   homepage "https://github.com/data-tamer/agentdev"
-  url "https://registry.npmjs.org/@datatamer.ai/agentdev/-/agentdev-1.0.14.tgz"
-  sha256 "f4a8531d8af0fd0442c98aab3898292d91c5f3e39e9024dd0b2a0b0ee3316cab"
+  url "https://registry.npmjs.org/@datatamer.ai/agentdev/-/agentdev-1.0.18.tgz"
+  sha256 "08416d9adb3d1b4b719bdd3e636cfe77e27b0db47d6bea2f9978e743e1447ecd"
   license "MIT"
 
   depends_on "node@22"
